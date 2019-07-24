6420e22edbe1bb8ad7439a051b2d238669523158
byzz <byzz@inven.co.kr>
2019-07-24 12:41:05 +0900

    .env editor 3 buttons

--- src/components/EnvEditor.js
+++ src/components/EnvEditor.js
@@ -1,9 +1,15 @@
 import React, { PureComponent } from 'react'
 import AceEditor from 'react-ace'
+import Draggable from 'react-draggable'
 
 import {
   EnvEditorWrap,
-  TitleBar
+  EnvEditorBottom,
+  BttnApply,
+  BttnClose,
+  BttnSaveClose,
+  TitleBar,
+  Button
 } from './styles/EnvEditor.sc'
 import idotenv from '../assets/dotenv.png'
 
@@ -11,39 +17,74 @@ import "brace/mode/ruby";
 import "brace/theme/github";
 
 export default class EnvEditor extends PureComponent {
-  componentDidUpdate() {
-    // this.props.fOverlay(true)
+  state = {
+    "activeDrags": 0
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.onStart = this.onStart.bind(this)
+    this.onStop = this.onStop.bind(this)
+  }
+
+  onStart() {
+    let t = this.state.activeDrags
+    this.setState({ "activeDrags": ++t })
+  }
+
+  onStop() {
+    let t = this.state.activeDrags
+    this.setState({ "activeDrags": --t })
   }
 
   render() {
+    const dragHandlers = {
+      "onStart": this.onStart,
+      "onStop": this.onStop
+    }
+
     return (
-      <EnvEditorWrap showEditor={this.props.showEditor}>
-        <TitleBar icon={idotenv}>.env editor</TitleBar>
-        <AceEditor
-          width="670px"
-          height="700px"
-          placeholder=""
-          mode="ruby"
-          theme="github"
-          name="edtenv"
-          onLoad={this.onLoad}
-          onChange={this.onChange}
-          fontSize={13}
-          showPrintMargin={true}
-          showGutter={true}
-          highlightActiveLine={true}
-          value={this.props.dotenvText}
-          setOptions={
-            {
-              enableBasicAutocompletion: true,
-              enableLiveAutocompletion: true,
-              enableSnippets: false,
-              showLineNumbers: true,
-              tabSize: 2,
+      <Draggable handle="#handleMoveable" {...dragHandlers}>
+        <EnvEditorWrap showEditor={this.props.showEditor}>
+          <TitleBar id="handleMoveable" icon={idotenv}>.env editor</TitleBar>
+          <AceEditor
+            width="670px"
+            height="700px"
+            placeholder=""
+            mode="ruby"
+            theme="github"
+            name="edtenv"
+            onLoad={this.onLoad}
+            onChange={this.onChange}
+            fontSize={13}
+            showPrintMargin={true}
+            showGutter={true}
+            highlightActiveLine={true}
+            value={this.props.dotenvText}
+            setOptions={
+              {
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: true,
+                enableSnippets: false,
+                showLineNumbers: true,
+                tabSize: 2,
+              }
             }
-          }
-        />
-    </EnvEditorWrap>
+          />
+        <EnvEditorBottom>
+          <BttnClose>
+            <Button>Close</Button>
+          </BttnClose>
+          <BttnApply>
+            <Button>Apply</Button>
+          </BttnApply>
+          <BttnSaveClose>
+            <Button>Save Close</Button>
+          </BttnSaveClose>
+        </EnvEditorBottom>
+      </EnvEditorWrap>
+    </Draggable>
     )
   }
 }
--- src/components/styles/EnvEditor.sc.js
+++ src/components/styles/EnvEditor.sc.js
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const EnvEditorWrap = styled.div`
   position: fixed;
   top: 80px;
   left: 645px;
   width: 673px;
-  height: 736px;
+  height: auto;
   background-color: #eee;
   z-index: 1000;
   border: 1px solid #003056;
@@ -23,4 +23,52 @@ export const TitleBar = styled.div`
   text-indent: 30px;
   padding: 5px;
   cursor: move;
+`
+
+export const EnvEditorBottom = styled.ul`
+  position: relative;
+  overflow: hidden;
+  list-style: none;
+  padding: 5px;
+  margin: 0;
+  background-color: #003c6c;
+  border-color: #003c6c;
+  border-style: solid;
+  border-width: 0 1px 1px 1px;
+`
+
+export const ActionButtons = styled.li`
+  float: left
+`
+
+export const BttnApply = styled(ActionButtons)`
+  float: right;
+  & > button {
+    background-color: #f6a104;
+    border-color: #c17d00;
+  }
+`
+
+export const BttnClose = styled(ActionButtons)`
+  float: left;
+`
+
+export const BttnSaveClose = styled(ActionButtons)`
+  float: right;
+  margin-right: 5px;
+`
+
+export const Button = styled.button`
+  background-color: #0071cb;
+  border-color: #0078d7;
+  border-style: solid;
+  -webkit-font-smoothing: antialiased;
+  color: #FFF;
+  font-size: 14px;
+  font-weight: 400;
+  cursor: pointer;
+  display: inline-block;
+  height: 32px;
+  min-width: 80px;
+  padding: 4px 20px 6px;
 `
